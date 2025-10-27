13f5ae2298ede58f9b8a72876aa8e80a6782be6d
alimanbazarbekova <aliman.bazarbekova@alatoo.edu.kg>
2025-10-27 13:45:24 +0600

    1st assignment

--- main.dart
+++ main.dart
@@ -1,1 +1,35 @@
+class Item {
+  String name;
+  double price;
+
+  Item(this.name, this.price);
+}
+
+class Category {
+  String name;
+  List<Item> items;
+
+  Category(this.name, this.items);
+}
+
+void main() {
+  var phone = Item('Phone', 600);
+  var laptop = Item('Laptop', 1000);
+  var tshirt = Item('T-Shirt', 15);
+  var sneakers = Item('Sneakers', 100);
+
+  var electronics = Category('Electronics', [phone, laptop]);
+  var fashion = Category('Fashion', [tshirt, sneakers]);
+
+  // Store all categories in a list
+  var categories = [electronics, fashion];
+
+  // Display categories and items
+  for (var category in categories) {
+    print('Category: ${category.name}');
+    for (var item in category.items) {
+      print(' - ${item.name}: \$${item.price}');
+    }
+  }
+}
 
