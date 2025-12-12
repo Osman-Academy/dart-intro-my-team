06050197c9b84d3f528fd36f19c80f780e138422
bektur-kg <roadtothedreambeka81@gmail.com>
2025-12-12 11:49:51 +0600

    initial commit

--- dyncamic-square/dynamic_square/android/app/src/main/kotlin/com/example/dynamic_square/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dynamic_square
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- dyncamic-square/dynamic_square/lib/ui/interactive-square/interactive_square_demo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dynamic_square/ui/interactive-square/interactive_square_demo_state.dart';
-import 'package:flutter/material.dart';
-
-class InteractiveSquareDemo extends StatefulWidget {
-  const InteractiveSquareDemo({Key? key}) : super(key: key);
-
-  @override
-  State<InteractiveSquareDemo> createState() => InteractiveSquareDemoState();
-}
