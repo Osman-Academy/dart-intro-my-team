f923a0902257681f985f8b5172bf700961f1064a
Sanzhar <snjmbj099@gmail.com>
2025-12-13 00:39:54 +0600

    feat: added supabase and getit locator

---
+++ expense-tracker/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:expensetracker/common/di/service_locator.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:get_it/get_it.dart';
+
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+      url: 'https://uvsfvhlvaosihhkqvmac.supabase.co',
+      anonKey: 'sb_publishable_yG4P7uT2oEpOTgtFYbvn7g_nEp9nKGo',
+  );
+
+  Supabase.instance.client.rest.headers.addAll({'apikey':'sb_publishable_yG4P7uT2oEpOTgtFYbvn7g_nEp9nKGo'});
+  setupLocator();
+
+  runApp(const MyApp());
+}
+
+final SupabaseClient supabase = GetIt.instance<SupabaseClient>();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+
+      initialRoute: '/auth',
+      routes: {
+      },
+    );
+  }
+}
+
+
