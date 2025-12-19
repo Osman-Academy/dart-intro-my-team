95e479c8ebe08bac5592b8a2f956a061c3cdc0cd
AkbekovNursultan <nursultan20052003@gmail.com>
2025-12-19 16:05:32 +0600

    Add MovieCard widget for displaying movie details

---
+++ lib/features/pages/movie_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:movie_search/models/movie.dart';
+
+class MovieCard extends StatelessWidget {
+  final Movie movie;
+  const MovieCard({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 2 / 3,
+            child: Image.network(
+              movie.safePoster,
+              fit: BoxFit.cover,
+              errorBuilder: (_, __, ___) => Container(
+                color: Colors.grey[300],
+                child: const Icon(Icons.movie, size: 60),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movie.title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  '${movie.year} • ⭐ ${movie.imdbRating}',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
