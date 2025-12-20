764e9ac8eb0ccdac47aa34b18abadc75b63febb3
nursultanadilbekov <nursultanadilbekov5@gmail.com>
2025-12-20 22:41:17 +0600

    edited usecase and network folder

---
+++ Desktop/movie_search_app/lib/core/network/api_client.dart
@@ -0,0 +1,51 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import '../../features/movies/data/models/movie_model.dart';
+
+part 'api_client.g.dart';
+
+@module
+abstract class NetworkModule {
+  @lazySingleton
+  Dio get dio {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://api.themoviedb.org/3',
+        connectTimeout: const Duration(seconds: 30),
+        receiveTimeout: const Duration(seconds: 30),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      ),
+    );
+
+    dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+
+    return dio;
+  }
+
+  @lazySingleton
+  ApiClient apiClient(Dio dio) => ApiClient(dio);
+}
+
+@RestApi()
+abstract class ApiClient {
+  factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
+
+  @GET('/search/movie')
+  Future<MovieResponse> searchMovies(
+    @Query('api_key') String apiKey,
+    @Query('query') String query,
+    @Query('page') int page,
+  );
+
+  @GET('/movie/popular')
+  Future<MovieResponse> getPopularMovies(
+    @Query('api_key') String apiKey,
+    @Query('page') int page,
+  );
+}
