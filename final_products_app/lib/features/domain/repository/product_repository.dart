a9c4ca8c0edd2771f8c3d306d153f8d5b2a061f4
TormentedTraveler <esyashaba@gmail.com>
2025-12-23 23:08:50 +0600

    updated repo

--- final_products_app/lib/features/domain/repository/product_repository.dart
+++ final_products_app/lib/features/domain/repository/product_repository.dart
@@ -1,4 +1,5 @@
 import 'package:final_products_app/core/resources/data_state.dart';
+import 'package:final_products_app/features/data/dto/product_dto.dart';
 import 'package:final_products_app/features/domain/models/product.dart';
 
 abstract class ProductRepository {
